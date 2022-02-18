4a45dd83131fc431eff045bf213df56afce1790a
Sakti Suman Tripathy <65999640+SaktiSuman@users.noreply.github.com>
2022-02-19 00:50:50 +0530

    Create Use of sequence characters.java

---
+++ Use of sequence characters.java
@@ -0,0 +1,6 @@
+public class UseOfSequenceCharacters {
+    public static void main(String[] args) {
+        String my_letter = "Dear Sakti, \n\tYou are doing good in JAVA. \n\tThanks!";
+        System.out.println(my_letter);
+    }
+}
