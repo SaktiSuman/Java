6606eb21c71388b369ec1b98e5c4a061e31728bc
Sakti Suman Tripathy <65999640+SaktiSuman@users.noreply.github.com>
2022-02-19 00:52:55 +0530

    Create Income Tax calc.java

---
+++ Income Tax calc.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class IncomeTaxCalculator {
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        float tax = 0;
+        System.out.println("Enter your income in Lacs: ");
+        float income = ob.nextFloat();
+        if (income < 2.5){
+            tax = tax + 0;
+        }
+        else if(income > 2.5 && income < 5.0){
+            tax = tax + 0.05f * (income - 2.5f);
+        }
+        else if(income >= 5.0 && income < 10.0){
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.2f * (income - 2.5f);
+        }
+        else{
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.2f * (10.0f - 2.5f);
+            tax = tax + 0.3f * (income - 10.0f);
+        }
+        System.out.println("The tax to be paid: "+tax);
+    }
+
+}
