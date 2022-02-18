b38baaf36bcbb56209ff5765da42b87059789e91
Sakti Suman Tripathy <65999640+SaktiSuman@users.noreply.github.com>
2022-02-19 00:48:20 +0530

    Create String Replace methods.java

---
+++ String Replace methods.java
@@ -0,0 +1,6 @@
+public class StringReplaceMethod {
+    public static void main(String[] args) {
+        String name = "  Sakti Suman  Tripathy";
+        System.out.println(name.replace(" ", "_"));
+    }
+}
